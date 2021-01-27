93d7104459e19a70e6f51ae7e2812db1ae43c86b
Bavi <bavidhraa.shrrei@kanini.com>
2021-01-27 23:11:58 +0530

    Add project files.

---
+++ medium_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace medium_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emply = new Employee();
+            emply.Insert_emply_details();
+            emply.Search_and_find();
+            Console.ReadKey();
+        }
+    }
+}
